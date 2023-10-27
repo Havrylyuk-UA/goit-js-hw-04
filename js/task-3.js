ca0978d8269d7a23ab336168ade02cf101fe234c
Havrylyuk-UA <142251279+Havrylyuk-UA@users.noreply.github.com>
2023-10-27 19:18:40 +0300

    hw end

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,21 @@
-function filterArray(numbers, value) {
-    const arr = [];
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    changeUsername(newName) {
+        return this.username = newName;
+    },
+    updatePlayTime(hours) {
+        return this.playTime += hours;
+    },
+    getInfo() {
+       return ` ${this.username} has ${this.playTime} active hours!`
+    },
+};
 
-    for (const item of numbers)
-        
-        if (item > value) {
-        arr.push(item)
-        }
-    
-    return arr
-}
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
