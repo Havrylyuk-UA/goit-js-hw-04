e448c485fef85cb72bd36f7894e4eef7c7eabbd1
Havrylyuk-UA <142251279+Havrylyuk-UA@users.noreply.github.com>
2023-10-28 12:16:11 +0300

    Remove Boolean

--- js/task-1.js
+++ js/task-1.js
@@ -7,7 +7,7 @@ function isEnoughCapacity(products, containerSize) {
         total += item;
     }
 
-    return Boolean(total <= containerSize)
+    return total <= containerSize
 }
 
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
